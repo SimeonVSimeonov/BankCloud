043ecac1018187f0559cf55f3f2517b4d4e19f7c
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2019-06-19 16:53:31 +0300

    Add Identity and BankCloudDbContext whit first Migration for Identity

---
+++ src/BankCloud/BankCloud.Data/Context/BankCloudDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankCloud.Data.Context
+{
+    public class BankCloudDbContext : IdentityDbContext
+    {
+        public BankCloudDbContext(DbContextOptions<BankCloudDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
